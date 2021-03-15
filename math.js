22b2b70b5296380d6b0ed967c47cc31577cddb4f
twisstr <twisstr@g.crookcountyschools.org>
2021-03-15 08:37:21 -0700

    lab 50 completed

---
+++ math.js
@@ -0,0 +1,21 @@
+//addition
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+
+var e = a + b + C + d;
+console.log('var e');
+
+
+//subtraction
+console.log(5-3);
+//expect output of: 2
+
+//multiplication
+console.log(3 * 4 * 2);
+//expect output of: 24
+
+//division
+console.log(12 / 2)
+//expect output of: 6
